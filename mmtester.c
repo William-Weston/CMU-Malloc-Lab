7fb6ae283e82dfc8186f96021dcf472cc12e4c4c
William Weston <wjtWeston@protonmail.com>
2025-04-15 15:35:59 -0400

    Add file mmtester.c and description to mm.c

---
+++ mmtester.c
@@ -0,0 +1,20 @@
+/**
+ * @file    mmtester.c
+ * @author  William Weston (wjtWeston@protonmail.com)
+ * @brief   Simple tester and main
+ * @version 0.1
+ * @date    2025-04-15
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "memlib.h"
+#include "mm.h"
+
+#include <stddef.h>     // EXIT_SUCCESS
+
+int main()
+{
+
+   return EXIT_SUCCESS;
+}
