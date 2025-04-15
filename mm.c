2abe7794313150f7a620fe316e9cbe126ee37398
William Weston <wjtWeston@protonmail.com>
2025-04-15 14:41:02 -0400

    Add files mm.h and mm.c along with function declarations in mm.h
    	These files shall contain the implementation of an allocator using an implicit free list

---
+++ mm.c
@@ -0,0 +1,11 @@
+/**
+ * @file    mm.c
+ * @author  William Weston (wjtWeston@protonmail.com)
+ * @brief   Source file for mm.h
+ * @version 0.1
+ * @date    2025-04-15
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * Implementation of allocator using an implicit free list
+ */
---
+++ mm.h
@@ -0,0 +1,26 @@
+/**
+ * @file    mm.h
+ * @author  William Weston (wjtWeston@protonmail.com)
+ * @brief   Implementation of general purpose allocator
+ * @version 0.1
+ * @date    2025-04-15
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * @note    Adapted from CSAPP
+ */
+#ifndef __2025_04_15_MM_H__
+#define __2025_04_15_MM_H__
+
+#include <stdlib.h>           // size_t
+
+int   mm_init( void );
+
+void* mm_malloc( size_t size );
+void* mm_realloc( void* ptr, size_t size );
+void* mm_calloc( size_t num, size_t size );
+void  mm_free( void* ptr );
+
+void  mm_check_heap( int verbose );
+
+#endif   // __2025_04_15_MM_H__
