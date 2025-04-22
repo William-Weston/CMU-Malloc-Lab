bd4a567809f8924cedacd7b3231ea01200843975
William Weston <wjtWeston@protonmail.com>
2025-04-22 17:39:06 -0400

    Add file comment headers and includes to explicit_list.c & seg_list.c

---
+++ explicit_list.c
@@ -0,0 +1,11 @@
+/**
+ * @file    explicit_list.c
+ * @author  William Weston (wjtWeston@protonmail.com)
+ * @brief   Source file for mm.h that uses explicit free list implementation
+ * @version 0.1
+ * @date    2025-04-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "mm.h"
---
+++ seg_list.c
@@ -0,0 +1,11 @@
+/**
+ * @file    seg_list.c
+ * @author  William Weston (wjtWeston@protonmail.com)
+ * @brief   Source file for mm.h that uses segregated free list implementation
+ * @version 0.1
+ * @date    2025-04-22
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include "mm.h"
